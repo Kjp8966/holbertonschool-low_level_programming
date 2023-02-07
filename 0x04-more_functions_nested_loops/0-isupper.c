4b13f42c9e16f5c6e155cfbc35c6d9237cba31f9
root <kjpeerry369@yahoo.com>
2023-02-06 20:53:54 -0800

    checks for upper case letters or characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,12 @@
 /**
  * _isupper - checks for uppercase character
  * @c: Variable text
- * Return: Always 0.
+ * Return: 0 or 1
  */
 int_isupper(int c)
 {
   if (c >= 'A' && c <= 'Z')
-    {
       return (1);
-    }
   else
-    {
       return (0);
-    }
 }
