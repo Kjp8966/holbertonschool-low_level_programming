51c936622388db57d89192891147cf2beb67acd7
root <kjpeerry369@yahoo.com>
2023-02-06 20:29:11 -0800

    corretion of the upper and lower case alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase, and then in uppercase, followed by a 
  * new line
@@ -7,10 +6,11 @@
  */
 int main(void)
 {
-  int ch;
-  for (ch = 'a'; ch <= 'z'; ch++)
-    putchar(ch);
-  for (ch = 'A'; ch <= 'Z'; ch++)
-    putchar(ch);
-  return (0);
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
