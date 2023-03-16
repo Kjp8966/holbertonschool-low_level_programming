2fb28ae948e4f27dfb16688d89e17998d4e24e16
Kjp8966 <kjpeerry369@yahoo.com>
2023-03-16 16:55:25 -0700

    Update 0-Print-List

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,19 +20,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#include <unistd.h>
-
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 #endif
