9e03411e7eacb3c32dbeb2a2da21c6ddc899833a
Kjp8966 <kjpeerry369@yahoo.com>
2023-10-02 14:04:06 -0700

    base 16 corrected

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-  int n;
-  char ch;
-  for (n = 48; n < 58; n++)
-    {
-      putchar(n);
-    }
-  for (ch = 'a'; ch <= 'f'; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
-  return (0);
+int n;
+char ch;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
